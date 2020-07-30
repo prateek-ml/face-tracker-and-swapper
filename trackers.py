7cfcfe9816e6cfd604a54f41c64e1ef77acdc269
Prateek Bhardwaj <54992024+prateek-ml@users.noreply.github.com>
2020-07-30 15:32:33 +0530

    Updated face an eye cascade classifier

--- trackers.py
+++ trackers.py
@@ -1,16 +1,13 @@
 import cv2
-import numpy as np
-import rects
 import utils
+
 
 class Face(object):
     """Data on facial features: face, eyes, nose, mouth."""
 
     def __init__(self):
         self.faceRect = None
-        self.leftEyeRect = None
-        self.rightEyeRect = None
-        self.smileRect = None
+        self.EyeRect = None
     
 class FaceTracker(object):
     """A tracker for facial features: face, eyes, nose, mouth"""
@@ -22,40 +19,13 @@ class FaceTracker(object):
 
         self._faces = []
 
-        self._faceClassifier = cv2.CascadeClassifier('cascades/haarcascade_frontalface_alt.xml')
-        self._LeyeClassifier = cv2.CascadeClassifier('cascades/haarcascade_lefteye_2splits.xml')
-        self._ReyeClassifier = cv2.CascadeClassifier('cascades/haarcascade_righteye_2splits.xml')
-        self._smileClassifier = cv2.CascadeClassifier('cascades/haarcascade_smile.xml')
+        self._faceClassifier = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
+        self._eyeClassifier = cv2.CascadeClassifier('cascades/haarcascade_eye.xml')
     
     @property
     def faces(self):
         """The tracked facial features."""
         return self._faces
-
-
-    # Detecting a single face feature by using the classifier on an image slice
-    def _detectOneObject(self, classifier, image, rect, imageSizetoMinSizeRatio):
-        x, y, w, h = rect
-
-        x = int(x)
-        y = int(y)
-
-        w = int(w)
-        h= int(h)
-
-        minSize = utils.DividedWidthHeight(image, imageSizetoMinSizeRatio)
-        
-        subImage = image[y:y+h, x:x+w]
-
-        subRects = classifier.detectMultiScale(
-            subImage, self.scaleFactor, self.minNeighbors, self.flags, minSize)
-
-        if len(subRects) == 0:
-            return None
-        
-        subX, subY, subW, subH = subRects[0]
-
-        return (x+subX, y+subY, w+w+subW, h+subH)
 
     def update(self, image):
         """Update the tracked facial features."""
@@ -63,16 +33,12 @@ class FaceTracker(object):
         self._faces = []
 
         #Creating an equalized, grayscale variant of the image
-        if utils.isGray(image):
-            image = cv2.equalizeHist(image)
-        else:
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            image = cv2.equalizeHist(image)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         
-        minSize = utils.DividedWidthHeight(image,8)
+        minSize = (120,120)
 
         faceRects = self._faceClassifier.detectMultiScale(
-            image, self.scaleFactor, self.minNeighbors, self.flags)
+            gray, self.scaleFactor, self.minNeighbors, self.flags, minSize)
         
         if faceRects is not None:
             for faceRect in faceRects:
@@ -80,25 +46,15 @@ class FaceTracker(object):
                 face = Face()
                 face.faceRect = faceRect
 
-                x, y, w, h = faceRect
+                (x,y,w,h) = faceRect
 
-
-                #Seek an eye in the upper-left part of the face
-                searchRect = (x+w/7, y, w*2/7, h/2)
-                face.leftEyeRect = self._detectOneObject(
-                    self._LeyeClassifier, image, searchRect, 64
+                #Detect eyes in the face
+                minSize = (40,40)
+                eye_region = gray[y:y+h, x:w+w]
+                face.eyeRects = self._eyeClassifier.detectMultiScale(
+                    eye_region, self.scaleFactor, self.minNeighbors, self.flags, minSize
                 )
 
-                #Seek an eye in the upper-right part of the face
-                searchRect = (x+w/4, y+h/4, w*2/7, h/2)
-                face.rightEyeRect = self._detectOneObject(
-                    self._ReyeClassifier, image, searchRect, 64
-                )
-
-                # Seek a mouth in the lower-middle part of the face.
-                searchRect = (x+w/6, y+h*2/3, w*2/3, h/3)
-                face.smileRect = self._detectOneObject(
-                self._smileClassifier, image, searchRect, 16)
                 self._faces.append(face)
     
     def drawRects(self, image):
@@ -107,19 +63,20 @@ class FaceTracker(object):
         #White rectangles in a grayscale image
         if utils.isGray(image):
             faceColor = 255
-            leftEyeColor = 255
-            rightEyeColor = 255
-            smileColor = 255
         
         else:
             faceColor = (255, 255, 255)  #White
-            leftEyeColor = (0, 0, 255)   #Red
-            rightEyeColor = (255, 0, 0)  #Blue
-            smileColor = (0,255,0)       #Green
+            
         
         for face in self._faces:
-            rects.outlineRect(image, face.faceRect, faceColor)
-            rects.outlineRect(image, face.leftEyeRect, leftEyeColor)
-            rects.outlineRect(image, face.rightEyeRect, rightEyeColor)
-            rects.outlineRect(image, face.smileRect, smileColor)
+            (x, y, w, h) = face.faceRect
+            cv2.rectangle(image, (x,y), (x+w, y+h), faceColor, 2)
+
+            for eyeRect in face.eyeRects:
+                (ex, ey, ew, eh) = eyeRect
+                cv2.rectangle(image, (x+ex, y+ey), (x+ex+ew, y+ey+eh), (0,255,0), 2)
+
+
         
+
+
